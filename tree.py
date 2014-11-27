b7a30965ef9239b7090f6c5643bb64d2b14fda64
renzon <renzon@gmail.com>
2014-11-27 00:20:08 -0200

    breadth_first_traversal

--- tree.py
+++ tree.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Node():
     def __init__(self, value, left=None, right=None):
         self.value = value
@@ -5,12 +8,28 @@ class Node():
         self.right = right
 
     def deep_first(self):
+        yield self
         if self.left is not None:
-            yield self.left
             yield from self.left.deep_first()
         if self.right is not None:
-            yield self.right
             yield from self.right.deep_first()
+
+    def breadth_first(self):
+        queue=deque()
+        queue.append(self)
+        while queue:
+            node = queue.popleft()
+            yield node
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+
+
+
+
+
 
 
     def __repr__(self):
@@ -22,7 +41,7 @@ def height(tree):
         return 0
     return 1 + max(height(tree.left), height(tree.right))
 
-
+print('#### Heights')
 print(height(None))  # expected 0
 root = Node(0)
 print(height(root))  # expected 1
@@ -46,9 +65,20 @@ print(height(root))  # expected 4
 def deep_first_traversal(tree):
     if tree is None:
         return
-    print(tree)
     for node in tree.deep_first():
         print(node)
 
 
+print('############### Deep First Traversal (')
 deep_first_traversal(root)  # Expected 0, 1 , 2 , 3, 4, 5
+
+
+def breadth_first_traversal(tree):
+    if tree is None:
+        return
+    for node in tree.breadth_first():
+        print(node)
+
+
+print('############### Breadth First Traversal (')
+breadth_first_traversal(root)  # Expected 0, 1 , 2 , 3, 5, 4
