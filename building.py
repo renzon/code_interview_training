570349c3fbc545bfe2cee0f85c24dd02aaa8e050
renzon <renzon@gmail.com>
2015-11-25 14:51:44 -0200

    Initial invalid conditions

--- building.py
+++ building.py
@@ -4,29 +4,20 @@
 from unittest.case import TestCase
 
 
-def combinations_in_building(building_floors, consecutive_floors):
+def combinations_in_building(building_floors, consecutive_floors, floors_cluster):
     if building_floors < consecutive_floors:
         return 0
-    return building_floors // consecutive_floors
+    total_floor = consecutive_floors * floors_cluster
+    if total_floor > building_floors:
+        return 0
 
 
 class BuildingsTests(TestCase):
     def test_building_floors_less_then_consecutive(self):
-        consecutive_floors = 3
-        building_floors = 1
-        self.assertEqual(0, combinations_in_building(building_floors, consecutive_floors))
-        building_floors = 2
-        self.assertEqual(0, combinations_in_building(building_floors, consecutive_floors))
+        self.assertEqual(0, combinations_in_building(1, 3, 1))
+        self.assertEqual(0, combinations_in_building(2, 3, 1))
 
-    def test_1_consecutive_floor(self):
-        consecutive_floors = 1
-        building_floors = 1
-        self.assertEqual(1, combinations_in_building(building_floors, consecutive_floors))
-        building_floors = 2
-        self.assertEqual(2, combinations_in_building(building_floors, consecutive_floors))
-        building_floors = 3
-        self.assertEqual(3, combinations_in_building(building_floors, consecutive_floors))
-        building_floors = 4
-        self.assertEqual(4, combinations_in_building(building_floors, consecutive_floors))
-        building_floors = 5
-        self.assertEqual(5, combinations_in_building(building_floors, consecutive_floors))
+    def test_cluster_bigger_then_build(self):
+        self.assertEqual(0, combinations_in_building(1, 1, 2))
+        self.assertEqual(0, combinations_in_building(2, 1, 3))
+        self.assertEqual(0, combinations_in_building(8, 3, 3))
