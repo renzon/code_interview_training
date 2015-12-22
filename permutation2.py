dd39184bc6531618cdbac4dd99d2b5db633a8a78
renzon <renzon@gmail.com>
2015-12-22 13:14:00 -0200

    permutation with deep_first non recursive

--- permutation2.py
+++ permutation2.py
@@ -15,7 +15,7 @@ def perm(seq):
 
 
 def perm_breadth_first(seq):
-    stack = [(tuple(), tuple(seq))]
+    stack = [(tuple(), tuple(seq)), None]
 
     while stack:
         result, possibilities = stack.pop()
@@ -28,5 +28,19 @@ def perm_breadth_first(seq):
             yield result
 
 
-for p in perm_breadth_first('ABC'):
+def perm_deep_first(seq):
+    stack = [(tuple(), tuple(seq), 0)]
+    while stack:
+        result, possibilities, cursor = stack.pop()
+        if cursor == len(possibilities) and len(result) == len(seq):
+            yield result
+        elif cursor < len(possibilities):
+            stack.append((result, possibilities, cursor + 1))
+            current_possibility = possibilities[cursor]
+            result += (current_possibility,)
+            possibilities = tuple(filter(lambda p: p != current_possibility, possibilities))
+            stack.append((result, possibilities, 0))
+
+
+for p in perm_deep_first('ABC'):
     print(''.join(p))
