71e28fe6b2f35b9d5981f42db803269ff643f699
renzon <renzon@gmail.com>
2016-02-03 17:33:59 -0200

    Improving doc of merge

--- merge.py
+++ merge.py
@@ -5,7 +5,16 @@ def _le(left, right):
 def merge(left, right, cmp=_le):
     """
     Merges two ordered iterables (left and right) keeping result ordered according to cmp.
-    Ex:
+
+    :param left: iterable
+    :param right: iterable
+    :param cmp: callable which receive one element from left and another from right and returns the on to be merged.
+    Default is less or equal then. Ex:
+    def _le(left, right):
+        return left if left <= right else right
+    :return: merging result of left and right
+
+    Usage:
 
     >>> from merge import merge
     >>> print(list(merge([1, 2], [3, 4])))
@@ -14,14 +23,6 @@ def merge(left, right, cmp=_le):
     [0, 1, 2, 4]
     >>> print(list(merge(range(3), range(2,5))))
     [0, 1, 2, 2, 3, 4]
-
-    :param left: iterable
-    :param right: iterable
-    :param cmp: callable which receive one element from left and another from right and returns the on to be merged.
-    Default is less or equal then. Ex:
-    def _le(left, right):
-        return left if left <= right else right
-    :return: merging of left and right
     """
     left = iter(left)
     right = iter(right)
