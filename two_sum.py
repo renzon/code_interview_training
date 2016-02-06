fa133e621519ec76c68375a40132653d63e8b009
renzon <renzon@gmail.com>
2016-02-06 03:11:50 -0200

    Two sum solved

--- two_sum.py
+++ two_sum.py
@@ -1,5 +1,5 @@
 # Problem http://www.programcreek.com/2012/12/leetcode-solution-of-two-sum-in-java/
-from bisect import bisect
+from bisect import bisect, bisect_left
 from unittest.case import TestCase
 
 
@@ -23,10 +23,9 @@ def two_sum(ints, sum):
     while start < end:
         wanted = sum - ints[start]
         previous = bisect(ints, wanted, start, end) - 1
-        if previous >= 0:
-            if wanted == ints[previous] and start != previous:
-                return start + 1, previous + 1
-        start = max(start + 1, bisect(ints, sum - ints[end - 1], start + 1, previous + 1) - 1)
+        if previous >= 0 and wanted == ints[previous] and start != previous:
+            return start + 1, previous + 1
+        start = max(start + 1, bisect_left(ints, sum - ints[end - 1], start + 1, previous + 1) - 1)
         end = previous + 1
 
     raise NotFound()
@@ -50,6 +49,12 @@ class Tests(TestCase):
 
     def test_middle(self):
         self.assertEquals((7, 10), two_sum(range(10), 15))
+
+    def test_repeated(self):
+        self.assertEquals((2, 3), two_sum([0, 2, 2], 4))
+
+    def test_trick_when_sum_minus_element_equals_to_element(self):
+        self.assertRaises(NotFound, two_sum, [0, 4], 0)
 
     def test_not_found(self):
         self.assertRaises(NotFound, two_sum, [1, 10], 1)
