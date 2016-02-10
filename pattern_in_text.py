cdf01f1c1b69527fe69b2ce803196f92b0af96bc
renzon <renzon@gmail.com>
2016-02-10 17:48:53 -0200

    KMP implementation

--- pattern_in_text.py
+++ pattern_in_text.py
@@ -2,7 +2,13 @@
 import unittest
 
 
-def search(pattern, text):
+def search_naive(pattern, text):
+    """
+    pattern matching with O(nm) time complexity where n=len(text) and m=len(pattern)
+    :param pattern:
+    :param text:
+    :return:
+    """
     start = 0
 
     def remaining(start_):
@@ -16,6 +22,62 @@ def search(pattern, text):
         else:
             return start
     return -1
+
+
+def search(pattern, text):
+    """
+    KMP algorithm with O(n+m) time complexity
+    :param pattern:
+    :param text:
+    :return:
+    """
+    n = len(text)
+    m = len(pattern)
+
+    if m == 0:
+        return 0
+
+    def calc_fail():
+        fail = [0] * m
+        i = 0
+        j = 1
+        while j < m:
+            if pattern[i] == pattern[j]:
+                fail[j] = fail[j - 1] + 1
+                j += 1
+                i += 1
+            elif i > 0:
+                i = fail[i - 1]
+            else:
+                i = 0
+                j += 1
+        return fail
+
+    fail = calc_fail()
+
+    i = 0
+    j = 0
+    while (n - i) >= (m - j):
+        if pattern[j] == text[i]:
+            if j == (m - 1):
+                return i - j
+            i += 1
+            j += 1
+        elif j > 0:
+            j = fail[j - 1]
+        else:
+            j = 0
+            i += 1
+
+    return -1
+
+
+#
+# i = 3
+# j = 1
+# acab
+
+print(search('amalgamation', ''))
 
 
 class Test(unittest.TestCase):
