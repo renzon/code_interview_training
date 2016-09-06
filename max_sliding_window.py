5872d973d429973d10dfe4df7e9ca1d794119bd2
renzon <renzon@gmail.com>
2016-09-06 09:57:46 -0300

    Max slide window with O(k**2+n*k), fixing for negative k and empty sequences

--- max_sliding_window.py
+++ max_sliding_window.py
@@ -1,18 +1,24 @@
 # First Approach
 from collections import deque
 
-import pytest
-
 
 def max_slide(seq, k):
-    if k == 0:
+    """Calculate de max slide window
+
+    running time O(k(k+1) + (n-2k)*k) if n>=k
+
+    :param seq: a sequence
+    :param k: a int
+    :return: max sliding window
+    """
+    if k <= 0:
         return []
     window = deque(maxlen=k)
     result = []
     for elt in seq:
         window.append(elt)
         result.append(max(window))
-    while True:
+    while window:
         window.popleft()
         if window:
             result.append(max(window))
@@ -21,10 +27,10 @@ def max_slide(seq, k):
     return result
 
 
-pytest
 
 
 def test_max_slide():
     assert [] == max_slide(range(3), 0)
+    assert [] == max_slide([], 3)
     assert list(range(3)) == max_slide(range(3), 1)
     assert list(range(3)) + [2] == max_slide(range(3), 2)
