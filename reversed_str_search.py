0d67a41d7cde88ba4670f28f0a24df8cce3bfb81
renzon <renzon@gmail.com>
2016-09-28 21:17:37 -0300

    reversed str search solved

--- reversed_str_search.py
+++ reversed_str_search.py
@@ -12,10 +12,10 @@ array or -1 otherwise.
 """
 from unittest.case import TestCase
 
-_PRIVATE_ARRAY = [str(i) for i in range(ord('a') + 1, ord('z'))]
+_PRIVATE_ARRAY = [chr(i) for i in range(ord('a') + 1, ord('z'))]
 
 
-def word(idx):
+def search_word(idx):
     try:
         return _PRIVATE_ARRAY[idx]
     except IndexError:
@@ -23,14 +23,33 @@ def word(idx):
 
 
 def word_index(word):
-    pass
+    # Find interval
+    end = 1
+    while True:
+        end_word = search_word(end)
+        if end_word == '' or end_word > word:
+            break
+        end *= 2
+    # Binary search
+    begin = end // 2
+    while begin < end - 1:
+        middle = (end + begin) // 2
+        current_word = search_word(middle)
+        if current_word == word:
+            return middle
+        elif current_word == '' or current_word > word:
+            end = middle
+        else:
+            begin = middle + 1
+    current_word = search_word(begin)
+    return begin if current_word == word else -1
 
 
 class WordIndexTestCase(TestCase):
     def test_words_in_array(self):
-        for i, v in _PRIVATE_ARRAY:
+        for i, v in enumerate(_PRIVATE_ARRAY):
             with self.subTest(v):
-                self.assertEqual(i, word_index(v))
+                self.assertEqual(i, word_index(v), 'Wrong for %s' % v)
 
     def test_word_before_first(self):
         self.assertEqual(-1, word_index('a'))
