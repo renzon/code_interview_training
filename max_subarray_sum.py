03bffb1e0217d87106293ed22e6b1a30b289b895
renzon <renzon@gmail.com>
2016-11-05 12:36:50 -0200

    Max subarray sum

---
+++ max_subarray_sum.py
@@ -0,0 +1,15 @@
+def max_subarray_sum(iterator):
+    iterable = iter(iterator)
+
+    def max_iter(current_sum, max_sum):
+        try:
+            next_element = next(iterable)
+        except StopIteration:
+            return max_sum
+        current_sum += next_element
+        return max_iter(max(current_sum, 0), max(current_sum, max_sum))
+
+    return max_iter(0, 0)
+
+
+print(max_subarray_sum([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
