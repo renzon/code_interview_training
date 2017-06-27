0e06e1d872ebc8edf1a2667296f95e15b9172378
renzon <renzon@gmail.com>
2017-06-27 20:39:32 -0300

    permuations with repetitions recursive solution

--- perm_with_rep.py
+++ perm_with_rep.py
@@ -1,13 +1,30 @@
+from collections import Counter
+
 import pytest
 
 
+def _perm_with_rep(current_s, chars_frequency):
+    if len(chars_frequency) == 0:
+        yield current_s
+    for char, freq in chars_frequency.items():
+        next_s = current_s + char
+        next_chars_frequency = dict(chars_frequency)
+        if freq == 1:
+            del next_chars_frequency[char]
+        else:
+            next_chars_frequency[char] -= 1
+        yield from _perm_with_rep(next_s, next_chars_frequency)
+
+
 def perm_with_rep(seq):
-    pass
+    chars_frequency = Counter(seq)
+    yield from _perm_with_rep('', chars_frequency)
 
 
 @pytest.mark.parametrize(
     'seq,expected',
     [
+        ('', {''}),
         ('1', {'1'}),
         ('12', set('12 21'.split())),
         ('123', set('123 312 231 321 132 213'.split())),
