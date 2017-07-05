81bd1f3d72d829624627a711e14246e3bfabf1f7
renzon <renzon@gmail.com>
2017-07-05 12:14:22 -0300

    Anagram in file: test read_chars reading ascii chars

--- anagram/anagram_in_file.py
+++ anagram/anagram_in_file.py
@@ -14,9 +14,19 @@ def ascii_file_name():
     return file_name
 
 
-def read_chars(file):
-    yield 1
+def read_chars(file_name):
+    with open(file_name, 'r', encoding='utf8') as file:
+        while True:
+            char = file.read(1)
+            if len(char) == 1:
+                yield char
+            else:
+                break
 
 
 def test_read_char_is_generator(ascii_file_name):
     assert isinstance(read_chars(ascii_file_name), GeneratorType)
+
+
+def test_read_ascii_chars(ascii_file_name):
+    assert string.ascii_letters == ''.join(read_chars(ascii_file_name))
