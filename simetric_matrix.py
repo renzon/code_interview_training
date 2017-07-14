e999c849c857ca46b6ef376436699e4132810207
renzon <renzon@gmail.com>
2017-07-14 17:30:34 -0300

    Simetric Matrix

--- simetric_matrix.py
+++ simetric_matrix.py
@@ -7,13 +7,8 @@ while True:
     size = int(input())
     if size == 0:
         break
-    elif not (0 < size < 100):
-        continue
 
     for i in range(size):
-        for j in range(size):
-            item = abs(i - j) + 1
-            print('{:>3}'.format(item), end="")
-        print()
+        elements = ('{:>3}'.format(abs(i - j) + 1) for j in range(size))
+        print(' '.join(elements))
     print()
-2
