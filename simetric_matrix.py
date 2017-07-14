666b868daa3cc16d95e1f563df5b9195c7405c76
renzon <renzon@gmail.com>
2017-07-14 17:50:22 -0300

    Simetric Matrix: pre calculating entires 100x 100 matrix to skip calculations

--- simetric_matrix.py
+++ simetric_matrix.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # https://www.urionlinejudge.com.br/judge/pt/problems/view/1478
+from itertools import islice
 
+matrix = []
+
+for i in range(101):
+    matrix.append(tuple('{:>3}'.format(abs(i - j) + 1) for j in range(101)))
 
 while True:
     size = int(input())
     if size == 0:
         break
 
-    for i in range(size):
-        elements = ('{:>3}'.format(abs(i - j) + 1) for j in range(size))
+    for line, _ in zip(matrix, range(size)):
+        elements = islice(line, size)
         print(' '.join(elements))
     print()
