8a8b599bcebfc914126bb73870e856754d80f243
renzon <renzon@gmail.com>
2017-07-14 18:21:43 -0300

    Simetric Matrix: making entire line, first place on uri judge online for python 3 with 0.032

--- simetric_matrix.py
+++ simetric_matrix.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # https://www.urionlinejudge.com.br/judge/pt/problems/view/1478
-from itertools import islice
 
-matrix = []
-
-for i in range(101):
-    matrix.append(tuple('{:>3}'.format(abs(i - j) + 1) for j in range(101)))
+matrix = tuple(' '.join('{:>3}'.format(abs(i - j) + 1) for j in range(101)) for i in range(101))
 
 while True:
     size = int(input())
     if size == 0:
         break
-
-    for line, _ in zip(matrix, range(size)):
-        elements = islice(line, size)
-        print(' '.join(elements))
+    width = 4 * size - 1
+    for i in range(size):
+        print(matrix[i][:width])
     print()
