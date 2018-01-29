cccd14233045f9696116ca3aaf45b5f9ddf52f61
renzon <renzon@gmail.com>
2018-01-29 15:39:18 -0200

    Implemented item addition on not full LRU

--- bet/lru.py
+++ bet/lru.py
@@ -1,12 +1,13 @@
 class LastResourceUsed:
     def __init__(self, limit=100):
-        self.limit = limit
+        self._limit = limit
+        self._items = 0
 
     def __setitem__(self, key, value):
-        pass
+        self._items += 1
 
     def __len__(self):
-        return 1
+        return self._items
 
 
 def test_lru_creation():
@@ -17,3 +18,10 @@ def test_increased_len_on_empty_lru():
     lru = LastResourceUsed(limit=2)
     lru['a'] = 'value'
     assert 1 == len(lru)
+
+
+def test_increased_len_not_full_lru():
+    lru = LastResourceUsed(limit=2)
+    lru['a'] = 'value a'
+    lru['b'] = 'value b'
+    assert 2 == len(lru)
