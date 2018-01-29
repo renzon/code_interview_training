4af3fd9ddfb95c6feb926ece21fa1ceeb3867edf
renzon <renzon@gmail.com>
2018-01-29 17:27:27 -0200

    Implemented first draw date test

---
+++ bet/date_calculation.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+import pytest
+
+SATURDAY_DRAW_DATETIME = datetime(2018, 1, 27, 20)
+WEDNESDAY_DRAW_DATETIME = datetime(2018, 1, 24, 20)
+
+
+def calculate_next_draw(dt: datetime = None):
+    dt = datetime.now() if dt is None else dt
+    return dt
+
+
+@pytest.mark.parametrize('dt', [WEDNESDAY_DRAW_DATETIME, SATURDAY_DRAW_DATETIME])
+def test_exactly_draw_datetime(dt):
+    assert dt == calculate_next_draw(dt)
