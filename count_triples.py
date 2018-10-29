82a1e07069d368f73ffaaa89e57fdf3d35f46ddc
renzon <renzon@gmail.com>
2018-10-29 17:21:00 -0300

    count_triples O(n) in time and memory. Solving ration==1

--- count_triples.py
+++ count_triples.py
@@ -1,11 +1,28 @@
 # https://www.hackerrank.com/challenges/count-triplets-1/problem
+import operator as op
 from collections import Counter
+from functools import reduce
 
 import pytest
 
 
+def ncr(n, r):
+    r = min(r, n - r)
+    numer = reduce(op.mul, range(n, n - r, -1), 1)
+    denom = reduce(op.mul, range(1, r + 1), 1)
+    return numer // denom
+
+
 def count_tripes(iterable, ratio):
     frequencies = Counter(iterable)
+    if ratio == 1:
+        total = 0
+        for i, freq in frequencies.items():
+            if freq > 2:
+                total += ncr(freq, 3)
+
+        return total
+
     total = 0
     for first, first_freq in frequencies.items():
         second = first * ratio
@@ -23,6 +40,7 @@ def count_tripes(iterable, ratio):
     [
         (2, 2, [1, 2, 2, 4]),
         (3, 6, [1, 3, 9, 9, 27, 81]),
+        (1, 5, [1, 1, 1, 1, 3, 3, 3]),
 
     ]
 
