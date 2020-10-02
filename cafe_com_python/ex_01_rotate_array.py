f402179340f60e882de3a25eb8b87cacf89ea0c2
renzon <renzon@gmail.com>
2020-10-02 09:30:16 -0300

    Exercício 1: Rotação Com função geradora. Tempo e Memória constante para se buscar primeiro elemento.

--- cafe_com_python/ex_01_rotate_array.py
+++ cafe_com_python/ex_01_rotate_array.py
@@ -47,15 +47,17 @@ Métodos em comum:
     >>> lst
     ['a', 'a', 'a', 'b', 'n', 'n']
 """
+from itertools import chain
+from time import time
 from typing import List
 
 
 def rotacionar(lista: List, k: int):
     """
     >>> lista = [1,2,3,4,5,6,7]
-    >>> list(rotacionar(lista, 3))
+    >>> list(rotacionar(lista, 3)) # O(n) em tempo, e a lista gerada O(n) em espaço
     [5, 6, 7, 1, 2, 3, 4]
-    >>> for elemento in rotacionar(lista, 1):
+    >>> for elemento in rotacionar(lista, 1): # O(n) em tempo, e a lista gerada O(n) em espaço
     ...     print(elemento)
     ...
     7
@@ -65,7 +67,7 @@ def rotacionar(lista: List, k: int):
     4
     5
     6
-    >>> next(iter(rotacionar(lista, 2)))
+    >>> next(iter(rotacionar(lista, 2))) # O(n) em tempo, e a lista gerada O(n) em espaço
     6
 
     :param iteravel:
@@ -75,3 +77,48 @@ def rotacionar(lista: List, k: int):
     primeira_fatia = lista[-k:]  # O(n) em tempo e em espaço
     segunda_fatia = lista[:-k]
     return primeira_fatia + segunda_fatia
+
+
+def rotacionar_generator(lista: List, k: int):
+    """
+    >>> lista = [1,2,3,4,5,6,7]
+    >>> list(rotacionar_generator(lista, 3))  # O(n) em tempo, e a lista gerada O(n) em espaço
+    [5, 6, 7, 1, 2, 3, 4]
+    >>> for elemento in rotacionar_generator(lista, 1):  # O(n) em tempo execução, O(1) em memória
+    ...     print(elemento)
+    ...
+    7
+    1
+    2
+    3
+    4
+    5
+    6
+    >>> next(iter(rotacionar_generator(lista, 2)))  # O(1) em tempo e em memória
+    6
+
+    :param iteravel:
+    :param k:
+    :return:
+    """
+    n = len(lista)
+    primeira_fatia_indices = range(n - k, n)
+    segunda_fatia_indices = range(n - k)
+    indices_rotacionados = chain(primeira_fatia_indices, segunda_fatia_indices)
+    for indice_rotacionado in indices_rotacionados:
+        yield lista[indice_rotacionado]
+
+
+if __name__ == '__main__':
+    auxiliar = 80_000_000
+    for tamanho in range(auxiliar, auxiliar+10_000+1, 10_000):
+        lista = list(range(tamanho))
+        inicio = time()
+        elemento = next(iter(rotacionar(lista, 1)))
+        final = time()
+        print(f'(Rotacionar) Tamanho {tamanho}: {elemento}, {final - inicio}')
+        inicio = time()
+        elemento = next(rotacionar_generator(lista, 1))
+        final = time()
+        print(f'(Rotacionar Geradora) Tamanho {tamanho}: {elemento}, {final - inicio}')
+        print()
