fa7484487bf1a2fa9fb4a4abe135c6114fd98bf8
renzon <renzon@gmail.com>
2020-11-26 21:00:56 -0300

    Exercício 5: Mediada de dois vetores ordenados
    Complexidade sub-linear e tempo
    constante em memória

--- cafe_com_python/ex_05_mediana_dois_vetores_ordenados.py
+++ cafe_com_python/ex_05_mediana_dois_vetores_ordenados.py
@@ -8,6 +8,87 @@ from itertools import islice
 from numbers import Number
 
 
+def encontrar_k_esimo_elemento(a, indice_inicial_de_a, indice_final_de_a, b, indice_inicial_de_b, indice_final_de_b, k):
+    m = indice_final_de_a - indice_inicial_de_a + 1  # 7
+    n = indice_final_de_b - indice_inicial_de_b + 1  # 11  k= 9
+    if m == 0:
+        return b[indice_inicial_de_b + k]
+    elif n == 0:
+        return a[indice_inicial_de_a + k]
+    if k == 0:
+        primeiro_elemento_de_a = a[indice_inicial_de_a]
+        primeiro_elemento_de_b = b[indice_inicial_de_b]
+        return min(primeiro_elemento_de_a, primeiro_elemento_de_b)
+
+    delta_para_o_meio_de_a = k * m // (m + n)
+    indice_do_meio_de_a = indice_inicial_de_a + delta_para_o_meio_de_a  # 3
+    indice_do_meio_de_b = indice_inicial_de_b + k - delta_para_o_meio_de_a - 1  # 5
+
+    elemento_do_meio_de_a = a[indice_do_meio_de_a]  # 4
+    elemento_do_meio_de_b = b[indice_do_meio_de_b]  # 6
+
+    if elemento_do_meio_de_b > elemento_do_meio_de_a:
+        k -= indice_do_meio_de_a - indice_inicial_de_a + 1
+        indice_inicial_de_a = indice_do_meio_de_a + 1
+        indice_final_de_b = indice_do_meio_de_b
+    else:
+        k -= indice_do_meio_de_b - indice_inicial_de_b + 1
+        indice_inicial_de_b = indice_do_meio_de_b + 1
+        indice_final_de_a = indice_do_meio_de_a
+    return encontrar_k_esimo_elemento(a, indice_inicial_de_a, indice_final_de_a, b, indice_inicial_de_b,
+                                      indice_final_de_b, k)
+
+
+
+def mediana_de_duas_listas_sub_linear(a: list, b: list) -> Number:
+    """
+    >>> mediana_de_duas_listas_sub_linear([1], [])
+    1
+    >>> mediana_de_duas_listas_sub_linear([], [2])
+    2
+    >>> mediana_de_duas_listas_sub_linear([1], [2])
+    1.5
+    >>> mediana_de_duas_listas_sub_linear([1], [2, 3000])
+    2
+    >>> mediana_de_duas_listas_sub_linear([1, 3], [2, 3000])
+    2.5
+    >>> mediana_de_duas_listas_sub_linear([1, 3, 4], [2, 3000])
+    3
+    >>> mediana_de_duas_listas_sub_linear([1, 3, 4], [2, 3000, 5000])
+    3.5
+    >>> mediana_de_duas_listas_sub_linear([1, 3, 4], [2, 3000, 5000, 5002])
+    4
+    >>> mediana_de_duas_listas_sub_linear([1, 3, 4], [2, 3000, 5000, 5002, 5004])
+    1502.0
+    >>> mediana_de_duas_listas_sub_linear([1, 3, 4, 4], [2, 3000, 5000, 5002, 5004])
+    4
+
+    Complexidade de tempo de execução:
+    Complexidade de memória:
+
+
+    :param a:
+    :param b:
+    :return:
+    """
+
+    # a=[1], b=[2]
+    m = len(a)
+    n = len(b)
+
+    tamanho_vetor_fundido = m + n
+
+    k = tamanho_vetor_fundido // 2
+    if tamanho_vetor_fundido % 2 == 1:
+        # retornar k-ésimo elmento
+        return encontrar_k_esimo_elemento(a, 0, m - 1, b, 0, n - 1, k)
+    else:
+        # retornar média entre o k-ésimo elemento e o (k-1)-ésimo elemento
+        k_esimo = encontrar_k_esimo_elemento(a, 0, m - 1, b, 0, n - 1, k)
+        k_esimo_menos_um = encontrar_k_esimo_elemento(a, 0, m - 1, b, 0, n - 1, k - 1)
+        return (k_esimo + k_esimo_menos_um) / 2
+
+
 def mediana_de_duas_listas_linear(a: list, b: list) -> Number:
     """
     >>> mediana_de_duas_listas_linear([1], [2])
