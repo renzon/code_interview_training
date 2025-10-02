059b06a340dd58288eaeea2b88a39a30882c0db8
renzo <renzo.n@gmail.com>
2025-10-02 13:56:10 -0300

    length_of_longest_substring Leetcode solution fix on two letter repeated

--- leetcode/length_of_longest_substring.py
+++ leetcode/length_of_longest_substring.py
@@ -14,15 +14,19 @@ def length_of_longest_substring(s: str) -> int:
     3
     >>> length_of_longest_substring('abaca')
     3
+    >>> length_of_longest_substring('tmmzuxt')
+    5
     """
 
     # O(n) in time and space
 
-    max_length = 0
+    max_length = 0 # 1, 2, 3, 4
     repeated_char_indexes: dict[str, int] = {}
-    start_idx = 0
+    start_idx = 0 # 0, 2
     for idx, char in enumerate(s):
         if char in repeated_char_indexes:
+            for idx_2 in range(start_idx, repeated_char_indexes[char]):
+                del repeated_char_indexes[s[idx_2]]
             start_idx = repeated_char_indexes[char] + 1
             repeated_char_indexes[char] = idx
         else:
