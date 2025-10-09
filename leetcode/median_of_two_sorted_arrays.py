dd990dd2ed8166638dfeacb50594458647535882
renzo <renzo.n@gmail.com>
2025-10-09 14:59:37 -0300

    Refactored Median of two sorted arrays

--- leetcode/median_of_two_sorted_arrays.py
+++ leetcode/median_of_two_sorted_arrays.py
@@ -18,38 +18,28 @@ class ListView:
     5
     >>> slc[0]
     2
-    >>> first_half, middle, second_half = l.break_in_middle()
+    >>> first_half, second_half = l.bissect()
     >>> first_half
-    [1, 2, 3]
-    >>> middle
-    [4]
+    [1, 2, 3, 4]
     >>> second_half
     [5, 6, 7]
-    >>> first_half, middle, second_half = l.break_in_middle(2)
+    >>> first_half, second_half = second_half.bissect()
     >>> first_half
-    [1, 2, 3]
-    >>> middle
-    [4, 5]
+    [5, 6]
     >>> second_half
-    [6, 7]
-    >>> first_half, middle, second_half = second_half.break_in_middle(2)
-    >>> first_half
-    [6]
-    >>> middle
     [7]
-    >>> second_half
-    []
-    >>> first_half, middle, second_half = first_half.break_in_middle(2)
+    >>> first_half, second_half = first_half.bissect()
     >>> first_half
-    []
-    >>> middle
+    [5]
+    >>> second_half
     [6]
+    >>> first_half, second_half = first_half.bissect()
+    >>> first_half
+    [5]
     >>> second_half
     []
-
-
-
     """
+
 
     def __init__(self, l: List, begin_index: int = 0, new_len: int = None):
         self._l = l
@@ -83,22 +73,9 @@ class ListView:
     def middle_index(self):
         return (len(self) + 1) // 2
 
-    def break_in_middle(self, middle_size=1):
-        return self.break_in(self.middle_index(), middle_size)
 
     def bissect(self):
         return self[:self.middle_index()], self[self.middle_index():]
-
-    def break_in(self, break_index: int, middle_size: int = 1):
-        if len(self) == 1:
-            return self[0:0], self[0:1], self[1:1]
-        elif len(self) == 2:
-            return self[:1], self[1:], self[2:2]
-        if break_index >= (len(self) - middle_size):
-            return self[0:0], self[:middle_size], self[middle_size:]
-
-        return self[:break_index], self[break_index: break_index + middle_size], self[
-            break_index + middle_size:]
 
 
 def find_median_sorted_arrays(nums1: List[int], nums2: List[int]) -> float:
@@ -168,46 +145,6 @@ def find_median_sorted_arrays(nums1: List[int], nums2: List[int]) -> float:
     j = (total_len) // 2
 
     while len(list_view_max) > 2 and list_view_min:
-        if len(list_view_max) < len(list_view_min):
-            list_view_max, list_view_min = list_view_min, list_view_max
-
-        # List with no intersection
-        if list_view_min[-1] < list_view_max[0]:
-            if j < len(list_view_min):
-                list_view_max = list_view_max[0: 0]
-                list_view_min = list_view_min[i: j + 1]
-            elif i >= len(list_view_min):
-                i -= len(list_view_min)
-                j -= len(list_view_min)
-                list_view_min = list_view_min[0: 0]
-                list_view_max = list_view_max[i: j + 1]
-            else:
-                list_view_min = list_view_min[i: i + 1]
-                list_view_max = list_view_max[:1]
-            delta = j - i
-            i = 0
-            j = delta
-            total_len = len(list_view_max) + len(list_view_min)
-            continue
-        elif list_view_min[0] > list_view_max[-1]:
-            if j < len(list_view_max):
-                list_view_min = list_view_min[0: 0]
-                list_view_max = list_view_max[i: j + 1]
-            elif i >= len(list_view_max):
-                i -= len(list_view_max)
-                j -= len(list_view_max)
-                list_view_max = list_view_max[0: 0]
-                list_view_min = list_view_min[i: j + 1]
-            else:
-                list_view_max = list_view_max[i: i + 1]
-                list_view_min = list_view_min[:1]
-            delta = j - i
-            i = 0
-            j = delta
-            total_len = len(list_view_max) + len(list_view_min)
-            continue
-        # End of list with no intersection logic
-
         min_first_half, min_second_half = list_view_min.bissect()
         max_first_half, max_second_half = list_view_max.bissect()
 
